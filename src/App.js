1d29f9179f76c44339ea39b52692eabee596c4ac
Anastasia R <anastasiaraffucci@gmail.com>
2023-07-04 13:32:18 -0400

    Added title puppie

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+        <h1>Puppies</h1>
         <img src={dog1} className="App-logo" alt="logo" />
+
     </div>
   );
 }
